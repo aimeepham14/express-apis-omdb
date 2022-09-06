8dff6c4932b34477227ac72032e74ac0d7be4b62
aimeepham14 <aimee.pham14@gmail.com>
2022-09-06 10:33:00 -0700

    edited

--- server.js
+++ server.js
@@ -19,29 +19,74 @@ app.use(ejsLayouts);
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
-console.log('process.env:', process.env.API_KEY)
-console.log(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.API_KEY}`)
+// console.log('process.env:', process.env.API_KEY)
+// console.log(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.API_KEY}`)
+
+
 // Routes
+//GET / --show search form for OMDB
 app.get('/', function(req, res) {
   // res.send('Hello, backend!');
   res.render('index.ejs')
 });
 
 
-app.get('/search', (req, res) => {
-  // console.log(req.query)
-  let searchTerm = req.query.search;
-  let url = "http://www.omdbapi.com/?s=" + searchTerm + `&apikey=${process.env.API_KEY}`;
-  axios.get(url)
-    .then(response => {
-      console.log(response.data)
-      res.render('results.ejs', {
-        input: req.query.userInput,
-        people: response.data.results
-      })
+// GET /results -- take in data from the search form, render search results from OMDB
+app.get('/results', (req, res) => {
+  console.log(req.query)
+    const userSearch = req.query.movieInput
+  //make a HTTP request to the api, supplying the user's input as a search query string 
+  axios.get(`http://www.omdbapi.com/?s=${userSearch}&apikey=${process.env.API_KEY}`)
+  //pass the data to the results template and render the data 
+  .then(response => {
+    console.log(response.data)
+    // res.json(response.data)
+    res.render('results.ejs', {
+      movies: response.data.Search,
+      // userSearch: userSearch
+      //shorthand
+      userSearch
     })
-    .catch(console.error)
+  })
+  .catch(err => {
+    console.log(err)
+    res.send('server error')
+  })
 })
+
+// GET ./movies/:id -- render a page of details about a single movie with an 
+app.get('/movies/:id', (req, res) => {
+  //make a request to the API and supply the :id as an imbdID
+  axios.get(`http://www.omdbapi.com/?i=${req.params.id}&apikey=${process.env.API_KEY}`)
+  //render a page with details for the movie
+  .then(response => {
+    res.json(response.data)
+    res.render('detail.ejs', {
+      deets: response.data
+    })
+  })
+  .catch(err => {
+    console.warn(err)
+    res.send('the server is upsetti spaghetti')
+  })
+  // res.send(`show the deets on movie with id: ${req.params.id}`)
+})
+
+
+
+
+//   let searchTerm = req.query.search;
+//   let url = "http://www.omdbapi.com/?s=" + searchTerm + `&apikey=${process.env.API_KEY}`;
+//   axios.get(url)
+//     .then(response => {
+//       console.log(response.data)
+//       res.render('results.ejs', {
+//         input: req.query.userInput,
+//         people: response.data.results
+//       })
+//     })
+//     .catch(console.error)
+// })
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 8000);
